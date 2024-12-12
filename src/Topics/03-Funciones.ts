285c671601e700d1cb3f507ecd0e391bb84857a4
Johancel <johancel131094@gmail.com>
2024-12-12 07:38:02 -0400

    funciones

---
+++ src/Topics/03-Funciones.ts
@@ -0,0 +1,8 @@
+ function addNumbers(a: number, b: number){
+ return a + b;
+ }
+
+ const resultado: number = addNumbers (1,2)
+  
+ console.log({ resultado})
+ ;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 //import './Topics/01-basic-types.ts';
-import './Topics/02-object-interface.ts';
+import './Topics/03-Funciones.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hola mundo
